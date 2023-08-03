b73d8bc47e5b96fda96bf1ebc2def6f59c6fe84e
Yoo Haechan <74089271+kwYoohae@users.noreply.github.com>
2023-08-03 17:28:22 +0900

    fix: CORS 설정 추가 (#36)

---
+++ backend/src/main/java/com/backend/topcariving/global/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package com.backend.topcariving.global.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMvcConfig implements WebMvcConfigurer {
+
+	private final long MAX_AGE_SECS = 3600;
+
+	@Override
+	public void addCorsMappings(final CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOriginPatterns("*")
+			.allowedMethods("*")
+			.allowedHeaders("*")
+			.allowCredentials(true)
+			.maxAge(MAX_AGE_SECS);
+	}
+}
