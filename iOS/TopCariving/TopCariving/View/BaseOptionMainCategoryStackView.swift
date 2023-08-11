2cfe44fd4c4faef1d9442926da65e38125ae0f1b
Eunno-An <12171647@inha.edu>
2023-08-12 01:01:29 +0900

    chore: BaseOptionMainCategory에 대한 View, StackView 파일 생성

---
+++ iOS/TopCariving/TopCariving/View/BaseOptionMainCategoryStackView.swift
@@ -0,0 +1,18 @@
+//
+//  BaseOptionMainCategoryStackView.swift
+//  TopCariving
+//
+//  Created by Eunno An on 2023/08/12.
+//
+
+import UIKit
+
+class BaseOptionMainCategoryStackView: UIStackView {
+    // MARK: - UI properties
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycles
+    
+    // MARK: - Helpers
+}
---
+++ iOS/TopCariving/TopCariving/View/BaseOptionMainCategoryView.swift
@@ -0,0 +1,18 @@
+//
+//  BaseOptionMainCategoryView.swift
+//  TopCariving
+//
+//  Created by Eunno An on 2023/08/12.
+//
+
+import UIKit
+
+class BaseOptionMainCategoryView: UIView {
+    // MARK: - UI properties
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycles
+    
+    // MARK: - Helpers
+}
