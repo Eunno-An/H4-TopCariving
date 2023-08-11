0946a529943bbd7e88654fb322e8f83f7973b76b
Eunno-An <12171647@inha.edu>
2023-08-12 01:48:30 +0900

    feat: BaseOptionMainCategoryView 추가

--- iOS/TopCariving/TopCariving/View/BaseOptionMainCategoryView.swift
+++ iOS/TopCariving/TopCariving/View/BaseOptionMainCategoryView.swift
@@ -9,10 +9,58 @@ import UIKit
 
 class BaseOptionMainCategoryView: UIView {
     // MARK: - UI properties
+    private var title: UILabel = {
+        let label: UILabel = UILabel()
+        label.text = "파워트레인/성능"
+        label.font = .designSystem(.init(name: .medium, size: ._14))
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    private var arrow: UIButton = {
+        let button: UIButton = UIButton()
+        button.setImage(UIImage(named: "arrow_down"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
     
     // MARK: - Properties
     
     // MARK: - Lifecycles
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUI()
+        setLayout()
+    }
     
     // MARK: - Helpers
+    func setUI() {
+        backgroundColor = .hyundaiLightSand
+        [title, arrow].forEach {
+            addSubview($0)
+        }
+    }
+    
+    func setLayout() {
+        NSLayoutConstraint.activate([
+            title.heightAnchor.constraint(equalToConstant: 46),
+            title.widthAnchor.constraint(equalToConstant: 89),
+            title.topAnchor.constraint(equalTo: topAnchor, constant: 12),
+            title.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 12),
+            
+            arrow.heightAnchor.constraint(equalToConstant: 24),
+            arrow.widthAnchor.constraint(equalToConstant: 24),
+            arrow.topAnchor.constraint(equalTo: topAnchor, constant: 11),
+            arrow.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 11)
+        ])
+    }
+    
+    func setTitle(to text: String) {
+        title.text = text
+    }
 }
