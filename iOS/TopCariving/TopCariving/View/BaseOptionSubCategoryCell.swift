d85f0b87e029b73b044f2814284df2bc25682b9f
Eunno-An <12171647@inha.edu>
2023-08-13 14:37:52 +0900

    feat: BaseOptionSubCategoryCell 구현. 클릭 이벤트 미완성

--- iOS/TopCariving/TopCariving/View/BaseOptionSubCategoryCell.swift
+++ iOS/TopCariving/TopCariving/View/BaseOptionSubCategoryCell.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class BaseOptionSubCategoryCell: UIView {
     // MARK: - UI properties
-    private let temp: UIView = {
-        let view: UIView = UIView()
-        view.backgroundColor = .hyundaiNeutral
-        view.layer.cornerRadius = 8
-        return view
+    private let imageView: UIImageView = {
+        let image: UIImage = UIImage(named: "baseOptionSample")!
+        let imageView: UIImageView = UIImageView(image: image)
+        imageView.layer.cornerRadius = 8
+        return imageView
+    }()
+    private let titleLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.text = "ISG 시스템"
+        label.font = .designSystem(.init(name: .regular, size: ._14))
+        return label
     }()
     // MARK: - Properties
     
@@ -32,17 +38,30 @@ class BaseOptionSubCategoryCell: UIView {
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        temp.frame = bounds
     }
     
     // MARK: - Helpers
     func setUI() {
-        addSubview(temp)
+        backgroundColor = .hyundaiNeutral
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(imageView)
+        addSubview(titleLabel)
     }
     
     func setLayout() {
         translatesAutoresizingMaskIntoConstraints = false
-        temp.translatesAutoresizingMaskIntoConstraints = false
         heightAnchor.constraint(equalToConstant: 71).isActive = true
+        
+        NSLayoutConstraint.activate([
+            imageView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
+            imageView.heightAnchor.constraint(equalToConstant: 55),
+            imageView.widthAnchor.constraint(equalToConstant: 93),
+            
+            titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 17),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
     }
 }
