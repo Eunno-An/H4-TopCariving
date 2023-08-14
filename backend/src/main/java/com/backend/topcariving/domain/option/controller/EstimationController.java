f939ff3c46b79674700e25f9fc70f4a7835991c4
Yoo Haechan <74089271+kwYoohae@users.noreply.github.com>
2023-08-14 13:31:23 +0900

    [BE] 내 차 만들기 결과를 요약보기로 통합 (#162)
    * fix: 카테고리 디테일을 넣어야 하는데 카테고리를 넣은 오류 발생
    
    * fix: 전체 결과 API를 요약 보기 API로 통합

--- backend/src/main/java/com/backend/topcariving/domain/option/controller/EstimationController.java
+++ backend/src/main/java/com/backend/topcariving/domain/option/controller/EstimationController.java
@@ -5,11 +5,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.backend.topcariving.domain.option.dto.request.esitmation.EstimationChangeRequestDTO;
-import com.backend.topcariving.domain.option.dto.response.archiving.ArchivingResponseDTO;
 import com.backend.topcariving.domain.option.dto.response.estimation.SummaryResponseDTO;
 import com.backend.topcariving.domain.option.service.EstimationService;
 import com.backend.topcariving.global.response.SuccessResponse;
@@ -26,13 +24,6 @@ public class EstimationController {
 
 	private final EstimationService estimationService;
 
-	@GetMapping("/")
-	@Operation(summary = "내 차 최종 결과 확인", description = "아카이빙 ID를 사용해 해당 차량의 최종 결과를 반환한다")
-	public ArchivingResponseDTO getArchivingResult(@RequestParam Long userId, @RequestParam Long archivingId) {
-
-		return estimationService.getArchivingResult(userId, archivingId);
-	}
-
 	@PostMapping("/")
 	@Operation(summary = "견적 요약보기에서 변경사항 저장", description = "견적 요약 보기")
 	public SuccessResponse<Long> estimationChange(@RequestBody EstimationChangeRequestDTO request) {
@@ -42,7 +33,7 @@ public class EstimationController {
 	}
 
 	@GetMapping("/summary/{userId}/{archivingId}")
-	@Operation(summary = "내 차 요약 보기 확인", description = "아카이빙 ID를 사용해서 현재 선택한 항목들의 요약보기를 진행한다")
+	@Operation(summary = "내 차 요약 보기 및 전체 결과 확인", description = "아카이빙 ID를 사용해서 현재 선택한 항목들의 요약보기 및 전체 결과 확인을 진행한다")
 	public SummaryResponseDTO summary(@PathVariable Long userId, @PathVariable Long archivingId) {
 		return estimationService.summary(userId, archivingId);
 	}
