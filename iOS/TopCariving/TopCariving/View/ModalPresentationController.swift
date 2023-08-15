5cbf7d5ae7c6479ffce43d21927665995a85a8bc
Eunno-An <12171647@inha.edu>
2023-08-15 09:16:18 +0900

    chore: 배경 색 버그 수정

--- iOS/TopCariving/TopCariving/View/ModalPresentationController.swift
+++ iOS/TopCariving/TopCariving/View/ModalPresentationController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ModalPresentationController: UIPresentationController {
     override var frameOfPresentedViewInContainerView: CGRect {
         guard let containerView = containerView else { return CGRect.zero }
-        containerView.backgroundColor = UIColor(cgColor: .init(red: 24/255, green: 24/255, blue: 24/255, alpha: 0.6))
+        containerView.backgroundColor = UIColor(cgColor: .init(red: 36/255, green: 36/255, blue: 36/255, alpha: 0.6))
         let modalSize = CGSize(width: containerView.bounds.width * 0.89, height: containerView.bounds.height * 0.387)
         let modalOrigin = CGPoint(
             x: (containerView.bounds.width - modalSize.width) / 2,
