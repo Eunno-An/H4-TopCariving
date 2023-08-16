5844d345787eef8b1f9b162f08d3ed45ce3438d4
Eunno-An <12171647@inha.edu>
2023-08-17 07:20:21 +0900

    feat: TicketView drawTicket() 추가, layoutSubviews에서 호출

--- iOS/TopCariving/TopCariving/View/MyCarComplete/TicketView.swift
+++ iOS/TopCariving/TopCariving/View/MyCarComplete/TicketView.swift
@@ -21,10 +21,32 @@ class TicketView: UIView {
         super.init(coder: coder)
         setUI()
     }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        drawTicket()
+    }
     // MARK: - Helpers
     private func setUI() {
         backgroundColor = .hyundaiGold
         layer.borderWidth = 1
         layer.borderColor = UIColor.hyundaiGold.cgColor
+    }
+    private func drawTicket() {
+        let ticketShapeLayer = CAShapeLayer()
+        ticketShapeLayer.frame = self.bounds
+        
+        let ticketShapePath = UIBezierPath(roundedRect: ticketShapeLayer.bounds, cornerRadius: 8)
+        let topLeftArcPath = UIBezierPath(arcCenter: CGPoint(x: 0, y: 100), radius: 30/2, startAngle: CGFloat(Double.pi / 2), endAngle: CGFloat(Double.pi + Double.pi / 2), clockwise: false)
+        topLeftArcPath.close()
+        
+        let topRightArcPath = UIBezierPath(arcCenter: CGPoint(x: ticketShapeLayer.frame.width, y: 100), radius: 30/2, startAngle: CGFloat(Double.pi / 2), endAngle: CGFloat(Double.pi + Double.pi / 2), clockwise: true)
+        topRightArcPath.close()
+        
+        ticketShapePath.append(topLeftArcPath)
+        ticketShapePath.append(topRightArcPath.reversing())
+        
+        ticketShapeLayer.path = ticketShapePath.cgPath
+        
+        layer.addSublayer(ticketShapeLayer)
     }
 }
