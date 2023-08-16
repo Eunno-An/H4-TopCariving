f613711c8a106d5cd505f39e2772e83e806dc0fa
Eunno-An <12171647@inha.edu>
2023-08-17 07:09:43 +0900

    feat: TicketView 속성 추가

--- iOS/TopCariving/TopCariving/View/MyCarComplete/TicketView.swift
+++ iOS/TopCariving/TopCariving/View/MyCarComplete/TicketView.swift
@@ -5,4 +5,26 @@
 //  Created by Eunno An on 2023/08/17.
 //
 
+import UIKit
 
+class TicketView: UIView {
+    // MARK: - UI properties
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycles
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+    }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUI()
+    }
+    // MARK: - Helpers
+    private func setUI() {
+        backgroundColor = .hyundaiGold
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.hyundaiGold.cgColor
+    }
+}
