e30c6d51a341a15e93eecb04df5b3c33e61372f6
Eunno-An <12171647@inha.edu>
2023-08-17 13:59:08 +0900

    feat: MyCarFeatureView속성 추가

--- iOS/TopCariving/TopCariving/View/MyCarComplete/MyCarFeatureView.swift
+++ iOS/TopCariving/TopCariving/View/MyCarComplete/MyCarFeatureView.swift
@@ -5,4 +5,41 @@
 //  Created by Eunno An on 2023/08/16.
 //
 
-import Foundation
+import UIKit
+
+class MyCarFeatureView: UIView {
+    // MARK: - UI properties
+    private var title: UILabel = {
+        let label = UILabel()
+        label.text = "나의 펠리세이드는 이런 기능을 가지고 있어요"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .designSystem(.init(name: .medium, size: ._18))
+        return label
+    }()
+    private var summaryContainerView = FeatureSummaryContainerView()
+
+    // MARK: - Properties
+    
+    // MARK: - Lifecycles
+    
+    // MARK: - Helpers
+    func setUI() {
+        summaryContainerView.translatesAutoresizingMaskIntoConstraints = false
+        [title, summaryContainerView].forEach {
+            addSubview($0)
+        }
+    }
+    func setLayout() {
+        NSLayoutConstraint.activate([
+            title.topAnchor.constraint(equalTo: topAnchor),
+            title.bottomAnchor.constraint(equalTo: summaryContainerView.topAnchor),
+            title.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            title.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 16),
+            
+            summaryContainerView.topAnchor.constraint(equalTo: topAnchor, constant: 10),
+            summaryContainerView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            summaryContainerView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15),
+            summaryContainerView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 15),
+        ])
+    }
+}
