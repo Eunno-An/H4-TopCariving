5ed3a614a2174b5947bd1f3488777f4a2e9f32f4
Eunno-An <12171647@inha.edu>
2023-08-25 07:07:57 +0900

    chore: ArchivingReviewViewModel기본속성 추가

--- iOS/TopCariving/TopCariving/Presentation/Archiving/ViewModel/ArchivingReviewViewModel.swift
+++ iOS/TopCariving/TopCariving/Presentation/Archiving/ViewModel/ArchivingReviewViewModel.swift
@@ -5,4 +5,24 @@
 //  Created by Eunno An on 2023/08/25.
 //
 
+import Combine
 import Foundation
+
+class ArchivingReviewViewModel: ViewModelType {
+    // MARK: - Input
+    struct Input {
+        
+    }
+    // MARK: - Output
+    struct Output {
+        
+    }
+    // MARK: - Dependency
+    
+    // MARK: - LifeCycle
+    
+    // MARK: - Helper
+    func transform(input: Input) -> Output {
+        Output()
+    }
+}
